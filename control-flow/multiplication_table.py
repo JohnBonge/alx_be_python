30d852ac10cf3434ac961d1762f58f64799a271b
JohnBonge <rjbigango@gmail.com>
2025-06-01 22:39:20 +0200

    added 10 to the multiplication table

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,6 +1,6 @@
 number =  int(input("Enter a number to see its multiplication table:"))
 
 x = 0
-for x in range(1,10):
+for x in range(1,11):
 	product = x * number
 	print(f"{number} * {x} = {product}")
